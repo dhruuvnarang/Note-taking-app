4f1e2cc0f6363e7e00a1f3f05f9c7088c3d529ae
Dhruv Narang <dhruv03420802820@bpitindia.com>
2022-07-07 23:04:05 +0530

    Initial commit

---
+++ src/components/Notes.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Notes() {
+  return (
+    <div className="note">
+      <h1>This is the react bootcamp</h1>
+      <p>
+        All the students who complete the Bootcamp with complete attendance and
+        project submission will be rewared certificate from us.
+      </p>
+    </div>
+  );
+}
+
+export default Notes;
